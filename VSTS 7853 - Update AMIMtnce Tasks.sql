ce05844157b55d50836952866645c81e60abff18
gpattersonm <38870103+gpattersonm@users.noreply.github.com>
2018-08-28 14:39:22 -0600

    Updates to make it just a SELECT statement.

--- VSTS 7853 - Update AMIMtnce Tasks.sql
+++ VSTS 7853 - Update AMIMtnce Tasks.sql
@@ -4,31 +4,13 @@ t.W6Key,
 t.CallID,
 t.OrderID,
 t.TaskType,
--- tt.Name AS TaskType,
--- spc.Name as SchedulePriorityCategory,
+tt.Name AS TaskType,
+spc.W6Key,
+spc.Name as SchedulePriorityCategory,
 t.[Status],
 ts.Name as Status
 from W6TASKS t
 JOIN W6TASK_TYPES tt on t.TaskType = tt.W6Key
--- JOIN W6SCHEDULEPRIORITYCATEGORY spc on t.BucketID = spc.W6Key
+JOIN W6SCHEDULEPRIORITYCATEGORY spc on tt.SchedulePriorityCategory = spc.W6Key
 JOIN W6TASK_STATUSES ts on t.[Status] = ts.W6Key
 where tt.Name = 'AMIMtnce'
-and ts.Name not in ('Completed', 'Cancelled', 'Incomplete')
-
-
-select * from W6SCHEDULEPRIORITYCATEGORY
-select * from W6CAPACITYBUCKET
-
-
-
--- Update the necessary tasks. Uncomment the below query after validating the proper keys
--- UPDATE W6Tasks
--- SET    ???????? = ???????, -- Large volume/window
---        Revision = Revision + 1, 
---        BinaryData = NULL 
--- WHERE  TaskType = 124821505 -- AMIMtnce
---        AND ???????? = ???????? -- Regular
---        AND status NOT IN ( 124135430, -- Completed  
---                              124143616, -- Cancelled  
---                              124145668 -- Incomplete  
---                             );
