b59ced3b66a07b297d1d82c6e2933126ef463ee6
gpattersonm <38870103+gpattersonm@users.noreply.github.com>
2018-10-11 11:57:54 -0600

    Added to Where clause

--- VSTS 7853 - Update AMIMtnce Tasks.sql
+++ VSTS 7853 - Update AMIMtnce Tasks.sql
@@ -3,6 +3,7 @@ SELECT
 t.W6Key,
 t.CallID,
 t.OrderID,
+t.SchedulingPriority,
 t.TaskType,
 tt.Name AS TaskType,
 spc.W6Key,
@@ -14,3 +15,4 @@ JOIN W6TASK_TYPES tt on t.TaskType = tt.W6Key
 JOIN W6SCHEDULEPRIORITYCATEGORY spc on tt.SchedulePriorityCategory = spc.W6Key
 JOIN W6TASK_STATUSES ts on t.[Status] = ts.W6Key
 where tt.Name = 'AMIMtnce'
+and t.DueDate > DATEADD(day,30,getdate())
