4bfde2aa3a2fe8e6c59eb6d7b1ccb153e397786d
gpattersonm <38870103+gpattersonm@users.noreply.github.com>
2018-10-19 12:20:43 -0600

    Added new query for 7688

---
+++ 7688-CheckOGM.sql
@@ -0,0 +1,8 @@
+select
+TimeCreated,
+Body
+from W6OUTGOING_MESSAGES 
+where TimeCreated > '2018-10-19 00:00:00' 
+-- and body like '%On Hold%'
+-- and body like '%OneWorld%'
+and body like '%DIABGP181019-1%'
